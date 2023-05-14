da10adff8636de244074245292ab076873cabc8d
solar-ml <dev30@protonmail.com>
2023-05-15 00:03:45 +0400

    cloudformation update

--- shell/cloudform.sh
+++ shell/cloudform.sh
@@ -1,10 +1,14 @@
 
 #!/bin/bash
-aws cloudformation create-stack --stack-name SSMParametersStack --template-body ../cloudformation/cfn-ssm.yaml
-aws cloudformation create-stack --stack-name S3BucketsStack --template-body ../cloudformation/cfn-s3.yaml
-aws cloudformation create-stack --stack-name StartStepFunctionDailyStack --template-body ../cloudformation/cfn-eventbridge_sf.yaml
+# aws cloudformation create-stack --stack-name SSMParametersStack --template-body ../cfn/cfn-ssm.yaml
+aws cloudformation create-stack --stack-name KafkaStreamsStack --template-body ../cfn/cfn-kafka-streams-msk.yaml
+aws cloudformation create-stack --stack-name FargateECSStack --template-body ../cfn/cfn-ecs-fargate.yaml
+aws cloudformation create-stack --stack-name SSMParamsS3BucketsStack --template-body ../cfn/cfn-s3-ssm.yaml
+aws cloudformation create-stack --stack-name StartStepFunctionDailyStack --template-body ../cfn/cfn-eventbridge_sf.yaml
 
-# https://github.com/aws-samples/emr-serverless-samples/blob/main/cloudformation/emr-serverless-cloudwatch-dashboard/README.md
+# https://github.com/aws-samples/emr-serverless-samples/blob/main/cfn/emr-serverless-cloudwatch-dashboard/README.md
+# replace <Application Id> with id of EMR Serverless application, created in AWS EMR Studio
+
 aws cloudformation create-stack --region <region> \
     --stack-name emr-serverless-dashboard \
     --template-body <file:///path/emr_serverless_cloudwatch_dashboard.yaml> \
